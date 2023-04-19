fb5038462d05e9566d716cc260909daaa07dfea4
Eddy Mena <developer.eddymena@gmail.com>
2023-04-18 19:59:11 -0600

    wiped app.jsx and created 2 new components form & header

---
+++ src/FormComponent.jsx
@@ -0,0 +1,9 @@
+import React from "react";
+
+export default function FormComponent(){
+    return(
+        <div>
+            <h1>Form :D</h1>
+        </div>
+    )
+}
---
+++ src/HeaderComponent.jsx
@@ -0,0 +1,9 @@
+import React from "react";
+
+export default function HeaderComponent() {
+    return (
+        <div>
+            <h1>Header :D</h1>
+        </div>
+    )
+}
