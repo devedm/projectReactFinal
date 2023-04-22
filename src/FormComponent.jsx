261434c142f9b0422017780ef11b22859f712fe8
Eddy Mena <developer.eddymena@gmail.com>
2023-04-22 17:52:38 -0600

    form added 2 fields

--- src/FormComponent.jsx
+++ src/FormComponent.jsx
@@ -1,9 +1,33 @@
-import React from "react";
+import React, { useState } from "react";
+import "./FormComponent.css";
 
-export default function FormComponent(){
-    return(
-        <div>
-            <h1>Form :D</h1>
-        </div>
-    )
+export default function FormComponent() {
+  const [patients, setPatients] = useState([]);
+  const [pet, setPet] = useState("");
+  const [owner, setOwner] = useState("");
+
+  function addPatient() {
+    const information = {
+      pet,
+      owner,
+    };
+    setPatients([...patients, information]);
+
+    setPet("");
+    setOwner("");
+  }
+
+  return (
+    <div>
+      <form className="inputForm" action="">
+        <label htmlFor="pet">Nombre Mascota</label>
+        <input id="pet" type="text" value={pet} onChange={(e) => { setPet(e.target.value)}}/>
+        <label htmlFor="owner">Nombre Dueño</label>
+        <input id="owner" type="text" value={owner} onChange={(e) => { setOwner(e.target.value)}}/>
+        <button onClick={addPatient} type="button">
+          Agregar
+        </button>
+      </form>
+    </div>
+  );
 }
